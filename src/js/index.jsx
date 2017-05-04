c8f6ae4e387715a6eb8387827f4623b0aef52c81
danivegamx <ingdanivega@gmail.com>
2017-05-04 16:16:15 -0500

    Preparando tercera sesión

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/App/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App cancion="Despacito.mp3"/>, document.getElementById('app'));
