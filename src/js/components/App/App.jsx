3330a66c88d430f6ea6738567ff9a4a27a6f6511
Juan Vazquez Manzo <Juan Vazquez Manzo>
2017-05-03 19:39:37 -0500

    Segundo dia

---
+++ src/js/components/App/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default class App extends React.Component
+{
+    constructor(props)
+    {
+      super(props);
+      this.state={reproduciendo:true}
+      this.pausar = this.pausar.bind(this);
+    }
+
+    pausar()
+    {
+      this.setState({
+        reproduciendo: !this.state.reproduciendo
+      })
+    }
+
+    render()
+    {
+      return (
+        <div>
+          Estas {(this.state.reproduciendo)? 'reproduciendo' : 'pausando'} {this.props.cancion}
+          <div onClick = { this.pausar}>  Pausa</div>
+        </div>
+      )
+    }
+}
